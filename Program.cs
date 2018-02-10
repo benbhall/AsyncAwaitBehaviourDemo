3506be7a5a7ef33bbc4128345cef1513be07a54a
benhall.io <benbhall@live.com>
2018-02-10 09:42:13 +0000

    First commit.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitBehaviourDemo
+{
+    internal class Program
+    {
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(" Demo 1: Awaiting call to long operation:");
+            MethodWithAwait().Wait();
+
+            Console.WriteLine(" Demo 2: NOT awaiting call to long operation:");
+            MethodWithoutAwait().Wait();
+
+            Console.ReadKey();
+        }
+
+        private static async Task MethodWithAwait()
+        {
+            Console.WriteLine(" MethodWithAwait() entered.");
+
+            Console.WriteLine(" Awaiting call to LongOperation().");
+            await LongOperation();
+
+            Console.WriteLine(" Pretending to do other work in MethodWithAwait()...");
+        }
+
+        private static async Task MethodWithoutAwait()
+        {
+            Console.WriteLine(" MethodWithoutAwait() entered.");
+
+            Console.WriteLine(" Call made to LongOperation() with NO await.");
+            Task task = LongOperation();
+
+            Console.WriteLine(" Do some other work in MethodWithoutAwait() after calling LongOperation()...");
+        }
+
+        private static async Task LongOperation()
+        {
+            Console.WriteLine(" LongOperation() entered.");
+
+            Console.WriteLine(" Starting the long (3 second) process in LongOperation()...");
+            await Task.Delay(4000);
+            Console.WriteLine(" Completed the long (3 second) process in LongOperation()...");
+        }
+    }
+}
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # AsyncAwaitBehaviourDemo
+
+Simple demo to accompany an article on (my blog) [http://benhall.io/], which I wrote to help junior .NET developers understand the behaviour of async/await. 
+
+Just a console application that calls two similar methods, which both in turn call a pretend long running operation.
+
+## MethodWithAwait()
+
+The aim here is to counter the surprisingly common misconception: that awaiting will create a thread that excutes the awaited method **in parallel** to the rest of the code continuing. When running this, expect the await to cause the code to block any continutation of MethodWithAwait() until the method we are waiting on is complete:
+
+![Animated GIF of async await demo](https://github.com/benbhall/AsyncAwaitBehaviourDemo/async_demo.gif).
+
+### MethodWithoutAwait()
+
+If you actually do want the behaviour some expect from the above i.e. you would like to leave the long operation Task completing and continue with MethodWithoutAwait(), then simply ommit the await. 
+
+You would get compiler warnings if you simply called without the await but you can prevent these, as I have in the code, by assigning the Task result to a variable.
+
+### Notes
+
+Both MethodWithAwait() and MethodWithoutAwait() return a Task type on which I used .Wait(), as I have not written this with a C# 7.1 async Main to make it more accessible at this time. This is critical on the call of MethodWithAwait() (once you appreciate the demos above you'll get this) as otherwise it would execute MethodWithoutAwait() in parallel and confuse the results!
